3feb2d930b35742c54a8849719025d4b20e1a4ae
pietro612 <pietroceroni@gmail.com>
2026-03-23 11:00:38 +0100

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,1 +1,12 @@
+let audio = new Audio("musica.mp3");
+let playing = false;
 
+function toggleAudio() {
+    if (!playing) {
+        audio.play();
+        playing = true;
+    } else {
+        audio.pause();
+        playing = false;
+    }
+}
